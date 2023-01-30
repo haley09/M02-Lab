cfbef04949e87dac6eed3dc1f0e0d31119f64507
Haley Abel <habel6@ivytech.edu>
2023-01-29 21:01:26 -0500

    first commit

---
+++ M02_Lab_StudentGPA.py
@@ -0,0 +1,21 @@
+# M02 Lab - Case Study: If...Else and while
+# This python code is going to ask for the students name and their GPAs along with letting them know if the qualify for the Dean's List ot the Honor Roll
+# This code is being written by Haley Abel
+
+lastName = input("Enter your last name here: ")
+
+if lastName == 'ZZZ':
+    quit()
+
+firstName = input("Enter your first name: ")
+
+studentGPA = float(input("Enter your GPA: "))
+
+if studentGPA  >= 3.5:
+    print("Congratulations " + firstName + " " + lastName + ", you made it into the Dean's List!")
+
+elif studentGPA >= 3.25:
+    print("Congratulations " + firstName + " " + lastName + ", you made it into the Honor Roll!")
+
+else:
+    print("Unfortunately " + firstName + " " + lastName + ", you did not make it into the Dean's list or Honor Roll, better luck next time.")
