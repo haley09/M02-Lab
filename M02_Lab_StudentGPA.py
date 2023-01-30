1ec7fa00ce2d656fc3e8203eefba946510392bc2
Haley Abel <habel6@ivytech.edu>
2023-01-29 21:50:46 -0500

    added a while statement

--- M02_Lab_StudentGPA.py
+++ M02_Lab_StudentGPA.py
@@ -1,21 +1,25 @@
 # M02 Lab - Case Study: If...Else and while
 # This python code is going to ask for the students name and their GPAs along with letting them know if the qualify for the Dean's List ot the Honor Roll
 # This code is being written by Haley Abel
+firstName = (str)
+lastName = (str)
+count = 1
+while count <=5:
+    print(count)
+    lastName = input("Enter your last name here: ")
+    firstName = input("Enter your first name: ")
+    studentGPA = float(input("Enter your GPA: "))
+    count = count + 1
 
-lastName = input("Enter your last name here: ")
+    if lastName == 'ZZZ':
+        quit()
 
-if lastName == 'ZZZ':
-    quit()
+    if studentGPA  >= 3.5:
+        print("Congratulations " + firstName + " " + lastName + ", you made it into the Dean's List!")
 
-firstName = input("Enter your first name: ")
+    elif studentGPA >= 3.25:
+        print("Congratulations " + firstName + " " + lastName + ", you made it into the Honor Roll!")
 
-studentGPA = float(input("Enter your GPA: "))
+    else:
+        print("Unfortunately " + firstName + " " + lastName + ", you did not make it into the Dean's list or Honor Roll, better luck next time.")
 
-if studentGPA  >= 3.5:
-    print("Congratulations " + firstName + " " + lastName + ", you made it into the Dean's List!")
-
-elif studentGPA >= 3.25:
-    print("Congratulations " + firstName + " " + lastName + ", you made it into the Honor Roll!")
-
-else:
-    print("Unfortunately " + firstName + " " + lastName + ", you did not make it into the Dean's list or Honor Roll, better luck next time.")
